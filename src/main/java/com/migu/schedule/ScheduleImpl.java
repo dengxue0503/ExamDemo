6b24ed27ea4e38a52017e5d45cc2b4d677e9599c
dengxue0503 <1755259724@qq.com>
2018-06-20 21:41:34 +0800

    dx

--- src/main/java/com/migu/schedule/ScheduleImpl.java
+++ src/main/java/com/migu/schedule/ScheduleImpl.java
@@ -1,63 +1,52 @@
 package com.migu.schedule;
 
-
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
-import com.sun.javafx.tk.Toolkit;
 
-import java.util.ArrayList;
+import javax.jws.WebService;
 import java.util.List;
 
-/*
-*类名和方法不能修改
- */
-public class Schedule {
+@WebService
+public class ScheduleImpl extends  Schedule {
 
-
+    @Override
     public int init() {
-        // TODO 方法未实现
-        TaskInfo taskinfo=new TaskInfo();
-        return ReturnCodeKeys.E001;
+
+        return ReturnCodeKeys.E000;
     }
 
-
+    @Override
     public int registerNode(int nodeId) {
         // TODO 方法未实现
         return ReturnCodeKeys.E000;
     }
-
+    @Override
     public int unregisterNode(int nodeId) {
         // TODO 方法未实现
         return ReturnCodeKeys.E000;
     }
 
-
+    @Override
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        if(taskId<0){
-            return ReturnCodeKeys.E009;
-        }
-
-        return ReturnCodeKeys.E010;
-    }
-
-
-    public int deleteTask(int taskId) {
-        // TODO 方法未实现
 
         return ReturnCodeKeys.E000;
     }
 
+    @Override
+    public int deleteTask(int taskId) {
+        // TODO 方法未实现
+        return ReturnCodeKeys.E000;
+    }
 
+    @Override
     public int scheduleTask(int threshold) {
         // TODO 方法未实现
         return ReturnCodeKeys.E000;
     }
 
-
+    @Override
     public int queryTaskStatus(List<TaskInfo> tasks) {
         // TODO 方法未实现
         return ReturnCodeKeys.E000;
     }
-
 }
